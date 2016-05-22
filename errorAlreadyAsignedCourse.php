bb42faff312b51af094360231c1a491b3965fc19
zdravkastrahinova <zdravkastrahinova1022@gmail.com>
2016-05-23 00:45:20 +0300

    Customize error messages

---
+++ errorAlreadyAsignedCourse.php
@@ -0,0 +1,12 @@
+<?php
+    require '/includes/header.php';
+    require '/includes/sidebar.php';
+?>
+
+    <div class="right">
+        <h3 class="error">You have already joined to this course.</h3>
+        <a class="redirect" href="user_courses.php">Back to My Courses</a> </br>
+        <a class="redirect" href="courses.php">Back to List Courses</a>
+    </div>
+
+<?php require '/includes/footer.php'?>
---
+++ userErrorMessage.php
@@ -0,0 +1,11 @@
+<?php
+    require '/includes/header.php';
+    require '/includes/sidebar.php';
+?>
+
+    <div class="right">
+        <h3 class="error">You cannot change administrator's credentials.</h3>
+        <a class="redirect" href="users.php">Back to List Users</a>
+    </div>
+
+<?php require '/includes/footer.php'?>
